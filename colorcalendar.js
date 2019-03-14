a2ca3d6f6113a5875160b131f6f66089ff4be371
Aurel Schwitter <aurel.schwitter@outlook.com>
2019-03-14 14:59:36 +0100

    Updated for more efficient color computation

--- colorcalendar.js
+++ colorcalendar.js
@@ -1,7 +1,7 @@
 /*
     Creator: Aurel Schwitter
-    Version: 1.0 
-    Date:    26.01.2017
+    Version: 1.2 
+    Date:    14.03.2019
 
     Integrate to Sharepoint:
     1) Upload to public Document library
@@ -14,93 +14,100 @@
 
 // Editable elements
 // Separators separating Name and Text
-var COL_CAL_SEPARATORS = [ ":",  "-", " "];
+const COL_CAL_SEPARATORS = [":", "-", " "];
 // Random values for different colors
-var CAL_RANDOM = 1.4;
-var CAL_RANDOM_2 = 5;
+const CAL_RANDOM = 99;
+const RED_MODIFIER = 0;
+const GREEN_MODIFIER = 0;
+const BLUE_MODIFIER = 0;
 
 _spBodyOnLoadFunctionNames.push("LoadColoredCalendar");
 
 // Waits for Sharepoint to load everything (1000ms)
-function LoadColoredCalendar (){
+function LoadColoredCalendar() {
     LoadSodByKey("SP.UI.ApplicationPages.Calendar.js", function () {
-        window.setTimeout(ColorCalendar, 1000);
+        window.setTimeout(ColorCalendar, 600);
     });
 }
 
 // Colores the calendar
-function ColorCalendar2() {
+function ColorCalendar() {
     // Get all calendar items
     var calitems = document.getElementsByClassName("ms-acal-item");
 
     // check if any exists
     if (calitems.length > 0) {
-        for (calitem in calitems){
-            var link = calitem.getElementsByTagName("a");
+        for (let calitem of calitems) {
+
+            var link = calitem.getElementsByTagName("a")[0];
             var text = link.innerHTML;
 
             // Is calendar already colored?
-            if ( text.indexOf(">") == -1){
+            if (!calitem.hasAttribute("data-calitem-colored")) {
 
                 // Get color from Name
                 var colors = GetColorCodeFromCategory(GetName(text));
                 // Convert color array to rgb(R,G,B)
                 var rgbcolors = "rgb(" + colors + ")";
-                            
+
                 // calculate black or white Text
                 var fcolor = GetForegroundColor(colors);
                 // Set foreground color
                 calitem.style.backgroundColor = rgbcolors;
-                text.style.color = fcolor;
-            };
+                link.style.color = fcolor;
+
+                // set attribute, it is already colored
+                calitem.setAttribute("data-calitem-colored", true)
+            } else {
+                // calendar is alredy colored, do not do again
+                return;
+            }
         };
 
     };
     // rerun this function in 2s
-    window.setTimeout(ColorCalendar, 1000);
+    window.setInterval(ColorCalendar, 1800);
 };
 
 // Generate number from string
-function HashCode (str) {
-    var hash = 0, i, chr, len;
-    if (str.length === 0) return hash;
-    for (i = 0, len = str.length; i < len; i++) {
-        chr = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
+function HashCode(str) {
+    let hash;
+    for (let i = 0; i < str.length; i++) {
+        hash = Math.imul(31, hash) + str.charCodeAt(i) | 0;
     }
-    return hash;
+    return hash & 0xffffff; // convert to number between 0x000000-0xfffff
 };
 
 // Get Name from category
 function GetName(originalText) {
-	var res = "";
-    COL_CAL_SEPARATORS.forEach(function(e) {
-        if (originalText.indexOf(e) > -1){
+    var res = "";
+    COL_CAL_SEPARATORS.forEach(function (e) {
+        if (originalText.indexOf(e) > -1) {
             res = originalText.split(e)[0].trim();
             return false;
         }
     });
-	return res === "" ? originalText : res;
+    return res === "" ? originalText : res;
 }
 
 function GetColorCodeFromCategory(category) {
-	if (category === undefined) return [0,0,0];
+    if (category === undefined) return [0, 0, 0];
 
     var hash = HashCode(category.trim());
-    hash = hash * CAL_RANDOM;
+    hash = hash * CAL_RANDOM / 7;
+
 
     // Convert to positive number
     // Randomize negative numbers 
     // Remove any decimal points
-    hash = Math.floor(hash < 0 ? Math.abs(hash / CAL_RANDOM_2) : hash);
+    hash = Math.floor(Math.abs(hash));
 
     // Convert decimal to r,g,b array
-    return [(hash & 0xff0000) >> 16, (hash & 0x00ff00) >> 8, (hash & 0x0000ff)];
+    return [((hash & 0xff0000) >> 16) + RED_MODIFIER, ((hash & 0x00ff00) >> 8) + GREEN_MODIFIER, (hash & 0x0000ff) + BLUE_MODIFIER];
 
 }
 
-function GetForegroundColor (rgb){
+function GetForegroundColor(rgb) {
     if (rgb.length != 3) return "#000000 !important";
     var rounded = Math.round((rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000);
     return rounded > 150 ? "#000000 !important" : "#ffffff !important";
